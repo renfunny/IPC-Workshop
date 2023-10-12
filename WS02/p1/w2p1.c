c169a37c30608be290da5235fd21e8612045c1b6
Cameron Gray <cameron.gray@senecacollege.ca>
2023-10-12 13:10:49 -0400

    Fixed missing readme for PY auto inputs

--- WS02/p1/w2p1.c
+++ WS02/p1/w2p1.c
@@ -1,25 +1,53 @@
-/*/////////////////////////////////////////////////////////////////////////
-                          Workshop - #2 (P1)
-Full Name  : 
-Student ID#: 
-Email      : 
-Section    : 
-
-Authenticity Declaration:
-
-I declare this submission is the result of my own work and has not been
-shared with any other student or 3rd party content provider. This submitted
-piece of work is entirely of my own creation.
-/////////////////////////////////////////////////////////////////////////*/
+//
+// Float issues, casting, rounding, working in cents...
+//
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
+/*  Canadian Coin Denominations
+    Coin    $val    Cents
+    ------- ------- -----------
+    Toonie  $2.00   200
+    Loonie  $1.00   100
+    Quarter $0.25    25
+    Dime    $0.10    10
+    Nickel  $0.05     5
+    Penny   $0.01     1
+*/
+
 int main(void)
 {
     const double TAX = 0.13;
-    const char patSize = 'S';
+    double price = 17.97;
+    int qty = 8;
+
+    // Rounding... for handling inaccurate floating-point values!
+    // Method-1:
+    //int subTotal = ((price * qty) * 100) + 0.5;
+    // Method-2:
+    int subTotal = ((price * qty) + 0.005) * 100;
+
+    double subTotalWrong = price * qty;
+    int coins, remaining;
+
+    printf("Product price     : $%.2lf\n", price);
+    printf("Number of products: %d\n", qty);
+    printf("Subtotal          : %d\n", subTotal);
+    // 3 ways to get the same result: make just 1 of the operands
+    //                                a double type (cast) and all
+    //                                will become double!
+    printf("Subtotal          : $%.2lf\n", (double)subTotal / 100);
+    printf("Subtotal          : $%.4lf\n", subTotal / 100.0);
+    //printf("Subtotal          : $%.4lf\n", subTotal / (double)100);
+
+    // Quarters:
+    coins = subTotal / 25;
+    //remaining = subTotal - (coins * 25); // NO!!! bad!! Horse!
+    remaining = subTotal % 25; // Use Modulus operator %!!!
+    printf("Quarters: %d (Remaining: %d)\n", coins, remaining);
+    printf("Quarters: %d (Remaining: $%.2lf)\n", coins, remaining/100.0);
 
     return 0;
 }
