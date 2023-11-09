2444573c714eff1a10b6dc91c7f4ea5233b277b4
Renato Cordova <renfunny@Renatos-MacBook-Air.local>
2023-11-09 11:37:00 -0500

    Finished W08 p1

--- WS08/p1/w8p1.h
+++ WS08/p1/w8p1.h
@@ -1,9 +1,9 @@
 /*/////////////////////////////////////////////////////////////////////////
                           Workshop - #8 (P1)
-Full Name  : 
-Student ID#: 
-Email      : 
-Section    : 
+Full Name  : Renato Cordova
+Student ID#: 153325238
+Email      : rcordova3@myseneca.ca
+Section    : NDD
 
 Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
@@ -13,8 +13,15 @@ piece of work is entirely of my own creation.
 
 
 // ----------------------------------------------------------------------------
+#define MAX_NUM_PRODUCTS 3
+#define SUGGESTED_SERVING 64
 // structures
-
+struct CatFoodInfo {
+    int sku;
+    double price;
+    int calories;
+    double weight;
+};
 
 
 
@@ -23,23 +30,23 @@ piece of work is entirely of my own creation.
 
 // 1. Get user input of int type and validate for a positive non-zero number
 //    (return the number while also assigning it to the pointer argument)
-
+int getIntPositive(int* num);
 
 // 2. Get user input of double type and validate for a positive non-zero number
 //    (return the number while also assigning it to the pointer argument)
-
+double getDoublePositive(double* num);
 
 // 3. Opening Message (include the number of products that need entering)
-
+void openingMessage(const int num_product);
 
 // 4. Get user input for the details of cat food product
-
+struct CatFoodInfo getCatFoodInfo(const int sequence_num);
 
 // 5. Display the formatted table header
-
+void displayCatFoodHeader(void);
 
 // 6. Display a formatted record of cat food data
-
+void displayCatFoodData(const int sku, const double* price, const int calories, const double* weight);
 
 // 7. Logic entry point
-
+void start();
